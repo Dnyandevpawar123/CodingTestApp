b57e7dc2c43c0249a4fd9285aa9ab239c4a37de5
Dnyandevpawar123 <109026923+Dnyandevpawar123@users.noreply.github.com>
2022-07-10 18:52:35 +0530

    Add files via upload
    -> Coding test is the simple java application
    -> Add Json file in resources folder name asjason.jason
    -> Instruction for compile and code run
       - Right click on coding test application and run as java application.
    -> Output
       - Both problem solutions shown in console

---
+++ CodingTest/src/main/java/com/example/CodingTest/controller/CodingTestController.java
@@ -0,0 +1,52 @@
+package com.example.CodingTest.controller;
+
+import java.io.FileReader;
+import java.io.IOException;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CodingTestController {
+
+	public static void main(String[] args) {
+		
+		JSONParser jsonParser =new JSONParser();
+		
+		try {
+			FileReader reader= new FileReader("src/main/resources/json.json");
+			Object obj =jsonParser.parse(reader);
+			JSONObject jsonObj =(JSONObject) obj;
+			JSONArray arr = (JSONArray) jsonObj.get("player");
+			System.out.println("Details of foreign Players");
+			for(int i=0;i<arr.size();i++)
+			{
+				JSONObject players =(JSONObject) arr.get(i);
+				if(!players.get("country").equals("India"))
+				{
+					System.out.println("Name:  "+players.get("name")+",  Country:"+players.get("country"));
+				}
+			}
+			System.out.println();
+			System.out.println("Detail of Wicket keepe");
+			for(int i=0;i<arr.size();i++)
+			{
+				JSONObject players =(JSONObject) arr.get(i);
+				if(players.get("role").equals("Wicket-keeper"))
+				{
+					System.out.println("Name:  "+players.get("name")+"  Role:"+players.get("role"));
+				}
+			}
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
